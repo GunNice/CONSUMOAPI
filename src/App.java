b5332583354c72b69ac67d6da9ed87ad73280334
GunNice <gunnicerca@gmail.com>
2023-06-25 14:56:20 -0300

    .

--- src/App.java
+++ src/App.java
@@ -9,7 +9,7 @@ public class App {
         System.out.println("===== API de consulta dos jogadores do Campeonato brasileiro Série A ====");
         System.out.println("========= Digite o ID do Jogador ex: 564, 654, 800, 1000, 5000: =========");
         System.out.println("=========================================================================\n\n");
-        System.out.println("-------------------------  INFORME A ID DESEJADA -----------------------:\n");
+        System.out.println("-------------------------  INFORME A ID DESEJADA -----------------------:");
         String idJogador = sc.nextLine();
         Fifa api = new Fifa();
         String json = api.buscaJogador(idJogador);
